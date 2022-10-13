d306f7f9f07e8fbffe5b27c2df901750d9acf85f
kishore951 <kishoresakthivel951@gmail.com>
2022-10-13 16:11:42 +0530

    again

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -9,6 +9,38 @@ export class AddemployeeComponent implements OnInit {
 
   constructor() { }
 
+  employeeCode=""
+  employeeName=""
+  designation=""
+  gender=""
+  companyName=""
+  salary=""
+  companyAddress=""
+  mobileNo=""
+  companyEmail=""
+  yearOfExperience=""
+  bloodGroup=""
+  dob=""
+
+  readValues=()=>{
+    let data={
+      "employeeCode":this.employeeCode,
+      "employeeName":this.employeeName,
+      "designation":this.designation,
+      "gender":this.gender,
+      "companyName":this.companyName,
+      "salary":this.salary,
+      "companyAddress":this.companyAddress,
+      "mobileNo":this.mobileNo,
+      "companyEmail":this.companyEmail,
+      "yearOfExperience":this.yearOfExperience,
+      "bloodGroup":this.bloodGroup,
+      "dob":this.dob
+
+    }
+    console.log(data)
+  }
+
   ngOnInit(): void {
   }
 
